b430c2488b21711d4dba545735fb8aa0ff47cec0
afamgit <nnajiafam@yahoo.com>
2023-12-07 20:56:55 +0100

    update user box
    update user box

--- app/components/user-box.tsx
+++ app/components/user-box.tsx
@@ -32,7 +32,6 @@ return (
 
     <div className='w-full min-h-screen flex justify-start bg-slate-100 items-start'>
     <div className='flex justify-start items-start'>
-    <ErrorBoundary fallback={<div>Something went wrong</div>}>
     <div className='h-screen sticky top-0 hidden md:h-full flex flex-shrink-0 md:block md:w-[250px] bg-neutral-800 text-white'>
     <div className="w-full flex flex-col justify-start items-start p-2 md:p-5">
     <div
@@ -42,6 +41,8 @@ return (
       </div>
         Orban Springs
     </div>
+    {/* <ErrorBoundary fallback={<div>Something went wrong</div>}>
+
    <div className="flex flex-col items-start my-2 py-2">
     {userInfo?.user.name} <form action={async() => {
       await signOut()
@@ -51,7 +52,9 @@ return (
       </button>
     </form>
 
-      </div> 
+      </div>     
+      </ErrorBoundary> */}
+
 
     </div>
     <div>
@@ -59,7 +62,6 @@ return (
 
       <AdminSideBar role={userprofile?.user?.role} />
     </div> 
-    </ErrorBoundary>
     </div>
   </div>
  
