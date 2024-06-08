0bc847152a975c55e03140c9b4eb27cd11519da7
afamgit <nnajiafam@yahoo.com>
2024-06-08 20:27:36 +0100

    Update pay-order-button-app.jsx

--- app/components/pay-order-button-app.jsx
+++ app/components/pay-order-button-app.jsx
@@ -75,7 +75,6 @@ const componentProps = {
 
                     {/* <p className='py-1 my-1'>{statusMsg}</p> */}
         {msg === 'ok' && btnref.current.click()}
-                    <p className='py-1 my-1'>{JSON.stringify(item)}</p>
 
         <p className='bg-light my-2 p-2'><a ref={btnref} href={redirecturl === 'exp' ? `exp://192.168.1.4:19000/--/order-details/${payref}` : `orban://order-details/${payref}`}>Exit window</a></p>
 
