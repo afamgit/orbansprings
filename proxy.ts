46661b7f9421c3bd37ba5df6ed7cda2821f689e1
afamgit <nnajiafam@yahoo.com>
2025-12-08 18:48:36 +0100

    Next upgrade

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,15 @@
 import NextAuth from 'next-auth';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { authConfig } from './auth.config';
  
 export default NextAuth(authConfig).auth;
+
+export function proxy(request: NextRequest) {
+  // Your proxy logic here
+  return NextResponse.next();
+}
+
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
--- scripts.ts
+++ scripts.ts
@@ -1,14 +1,23 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import "dotenv/config";
+import { PrismaClient } from '@/prisma/generated/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
+// Use a global variable to persist the Prisma Client instance across reloads in development
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5
+});
 
+// Instantiate the client:
+const prisma = global.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
+export { prisma };
